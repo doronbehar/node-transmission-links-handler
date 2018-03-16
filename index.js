d61b50cb9b53ea333ae6a138cfdb0740dbca1d7d
Doron Behar <doron.behar@gmail.com>
2018-03-16 13:54:06 +0200

    Use `null` instead of `undefined` for `args.auth`.

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ var combineArgumentsAndConfig = new Promise(function (resolve, reject) {
     }
   }
   var authentication;
-  if ((args.authenv || config.authenv) && args.auth === undefined) {
+  if ((args.authenv || config.authenv) && args.auth === null) {
     if (process.env.TR_AUTH) {
       authentication = process.env.TR_AUTH;
     } else {
